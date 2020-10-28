a1b16fb8d5201a69cf7556927ed5f374feef477c
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-28 11:44:17 -0300

    Métodos e referências em Java

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -2,14 +2,34 @@ package bytebank;
 
 public class TestaMetodo {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
+		Conta contaPedro = new Conta();
 		
-		System.out.println(conta.saldo);
+		contaPedro.saldo = 100;
+		System.out.println("Saldo - Conta Pedro: " + contaPedro.saldo);
 		
-		boolean conseguiuRetirar = conta.saca(20);
-		System.out.println(conta.saldo);
-		System.out.println(conseguiuRetirar);
+		System.out.println("Deposito - Conta Pedro: 50 temers");
+		contaPedro.deposita(50);
+		System.out.println("Saldo - Conta Pedro: " + contaPedro.saldo);
+		
+		System.out.println("\nSaque - Conta Pedro: 20 temers");
+		boolean conseguiuRetirar = contaPedro.saca(20);
+		System.out.println("Saque realizado: " + conseguiuRetirar);
+		System.out.println("Saldo - Conta Pedro: " + contaPedro.saldo);
+		
+		Conta contaMarcelo = new Conta();
+		contaMarcelo.deposita(1000);
+		System.out.println("\nSaldo - Conta Marcelo: " + contaMarcelo.saldo);
+		
+		System.out.println("\nTranferencia: 300 temers (Marcelo -> Pedro): ");
+		boolean sucessoTransferencia = contaMarcelo.transfere(300, contaPedro);
+		if(sucessoTransferencia) {
+			System.out.println("Tranferencia realizada com sucesso!");
+		} else {
+			System.out.println("Faltou dinheiro.");
+		}
+		
+		System.out.println("Saldo - Conta Marcelo: " + contaMarcelo.saldo);
+		System.out.println("Saldo - Conta Pedro: " + contaPedro.saldo);
+		
 	}
 }
