92113981ec6722ca7a2342f0cd77c2ccabe2d73c
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-28 11:11:57 -0300

    Retorno de métodos em Java

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -7,5 +7,9 @@ public class TestaMetodo {
 		conta.deposita(50);
 		
 		System.out.println(conta.saldo);
+		
+		boolean conseguiuRetirar = conta.saca(20);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 }
