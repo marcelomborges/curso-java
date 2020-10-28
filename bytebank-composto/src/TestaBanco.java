bc5282b10e8aeeaa6c70542379de6870607badc6
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-28 12:10:09 -0300

    Composição de objetos em Java

---
+++ bytebank-composto/src/TestaBanco.java
@@ -0,0 +1,15 @@
+
+public class TestaBanco {
+	public static void main(String[] args) {
+		Cliente marcelo = new Cliente();
+		marcelo.cpf = "111.111.111-11";
+		marcelo.nome = "Marcelo Borges";
+		marcelo.profissao = "Programador";
+		
+		Conta contaMarcelo = new Conta();
+		contaMarcelo.deposita(100);
+		
+		contaMarcelo.titular = marcelo;
+		System.out.println(contaMarcelo.titular.nome);
+	}
+}
