9136daf6b60c67cf2a97db4b5b18111e798cc85c
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-30 13:10:04 -0300

    Java - Criando e invocando exceções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -5,16 +5,11 @@ public class Fluxo {
         
         try {
         	metodo1();
-        } catch (ArithmeticException | NullPointerException ex) {
+        } catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
         	String msg = ex.getMessage();
         	System.out.println("Exception - " + msg);
         	ex.printStackTrace();
-        } 
-//        catch (NullPointerException ex) {
-//        	String msg = ex.getMessage();
-//        	System.out.println("NullPointerException - " + msg);
-//        	ex.printStackTrace();
-//        }
+        }
         
         System.out.println("Fim do main");
     }
@@ -22,29 +17,14 @@ public class Fluxo {
     private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
-//        try {
-//        	metodo2();
-//        } catch (ArithmeticException ex) {
-//        	System.out.println("ArithmeticException");
-//        }
-        
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-//            Conta c = null;
-//            c.deposita();
-            int a = i / 0;
-//            try {
-//            	int a = i / 0;
-//            } catch(ArithmeticException ex) {
-//            	System.out.println("ArithmeticException");
-//            }
-            
-        }
-        System.out.println("Fim do metodo2");
+        
+        throw new MinhaExcecao("Erro"); 
+        
+        //System.out.println("Fim do metodo2");
     }
 }
