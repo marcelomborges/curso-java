09b7639fdfa9294e9b6a90146560547571a7f78a
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-30 02:25:29 -0300

    Java - Tentativa de herança múltipla

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,5 +1,5 @@
 
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario {
 		
 	public double getBonificacao() {
 		System.out.println("-> Pegando bonificacao do GERENTE.");
