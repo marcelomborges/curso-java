e19799ce18fa7211963083eb41515f3ee2032611
Marcelo M. Borges <marcelomborges@outlook.com>
2020-10-30 02:12:36 -0300

    Java - Classe abstrata intermediária

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,25 +1,8 @@
 
-public class Gerente extends Funcionario {
-	private int senha;
-	
+public class Gerente extends FuncionarioAutenticavel {
+		
 	public double getBonificacao() {
 		System.out.println("-> Pegando bonificacao do GERENTE.");
 		return super.getSalario();
-	}
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public int getSenha() {
-		return senha;
 	}
 }
