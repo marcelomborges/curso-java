0e9869091ccf5aef04e53932d969a926308a06b2
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-04 09:43:08 -0300

    Java - Arrays

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,28 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayDePrimitivos {
+
+	public static void main(String[] args) {
+		
+		int[] idades = new int[5];
+		
+//		idades[0] = 29;
+//		idades[1] = 39;
+//		idades[2] = 49;
+//		idades[3] = 59;
+//		idades[4] = 69;
+//		
+//		System.out.println(idades[3]);
+//		System.out.println(idades.length);
+		
+		for(int i = 0; i < idades.length; i++) {
+			idades[i] = i*i;
+		}
+		
+		for(int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+
+	}
+
+}
