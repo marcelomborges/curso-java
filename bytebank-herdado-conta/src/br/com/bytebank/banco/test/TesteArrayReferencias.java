f48ce8df3c3e9f8a734798b42d076129849a6a0c
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-04 10:20:40 -0300

    Java - Type Cast e argumentos da main

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,25 +1,29 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.*;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];
+		//Conta[] contas = new Conta[5];
+		Object[] referencias = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(111, 111);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(222, 222);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(222, 222);
+		referencias[1] = cc2;
+		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
 		
 		System.out.println(cc2.getNumero());
-		System.out.println(contas[1].getNumero());
+		//System.out.println(referencias[1].getNumero());
 		
-		ContaCorrente ref = contas[1];
+		ContaPoupanca ref = (ContaPoupanca) referencias[1];//Type Cast
+		//ContaCorrente ref = (ContaCorrente) referencias[1];//ClassCastException
 		System.out.println(ref.getNumero());
-
 	}
 
 }
