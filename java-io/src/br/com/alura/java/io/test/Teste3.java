b00de9b7d2a09f6f0fbde9ba442286457512d04c
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-05 19:03:02 -0300

    Java - PrintStream e PrintWriter

--- java-io/src/br/com/alura/java/io/test/Teste3.java
+++ java-io/src/br/com/alura/java/io/test/Teste3.java
@@ -1,5 +1,18 @@
 package br.com.alura.java.io.test;
 
-public class Teste3 {
+import java.io.*;
 
+public class Teste3 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		//PrintStream ps = new PrintStream(new File("lorem2.txt"));
+		PrintWriter ps = new PrintWriter("lorem2.txt");
+		
+		ps.println("-> Teste de Escrita - Linha 1");
+		ps.println();
+		ps.println("-> Teste de Escrita - Linha 2");
+		
+		ps.close();
+	}
 }
