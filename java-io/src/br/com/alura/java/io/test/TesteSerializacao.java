a69b9ae5e3ff62c8f979fa1282fe9ead6617cf30
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-06 11:17:07 -0300

    Java - Serialização de Strings

---
+++ java-io/src/br/com/alura/java/io/test/TesteSerializacao.java
@@ -0,0 +1,27 @@
+package br.com.alura.java.io.test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class TesteSerializacao {
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		
+		String nome = "Marcelao";
+		
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+//		oos.writeObject(nome);
+//		oos.close();
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
+		String nome2 = (String) ois.readObject();
+		ois.close();
+		
+		System.out.println(nome2);
+		
+	}
+
+}
