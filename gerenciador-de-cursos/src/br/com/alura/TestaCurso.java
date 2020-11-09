4bda8c57af571b5c36b9becd40117e1161fdf614
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-09 13:56:40 -0300

    Java - Classe Curso e coleções

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,24 @@
+package br.com.alura;
+
+import java.util.List;
+
+public class TestaCurso {
+	
+	public static void main(String[] args) {
+		
+		Curso javaColecoes = new Curso("Dominando as colecoes do Java", "Paulo Silveira");
+		
+//		List<Aula> aulas = javaColecoes.getAulas();//mesma referencias para atributo aulas
+//		System.out.println(aulas);
+//		aulas.add(new Aula("Trabalhando com ArrayList",21));
+//		System.out.println(aulas);
+//		System.out.println(aulas == javaColecoes.getAulas());
+		
+//		javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList",21));
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList",21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula",20));
+		javaColecoes.adiciona(new Aula("Modelando com Coleções",22));
+		
+		System.out.println(javaColecoes.getAulas());
+	}
+}
