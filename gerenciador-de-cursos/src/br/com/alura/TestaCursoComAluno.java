3d3eaf7f2ebdef28bb0ee19cb996dd6feba9cecd
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-10 11:53:54 -0300

    Java - unmodifiableSet() e emptySet()

--- gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Collections;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -18,6 +21,17 @@ public class TestaCursoComAluno {
 		javaColecoes.matricula(a3);
 		
 		javaColecoes.getAlunos().forEach(aluno -> System.out.println("Aluno:" + aluno));
+		
+//		javaColecoes.getAlunos().forEach(aluno -> {
+//			System.out.println("Aluno:" + aluno);
+//		});
+		
+//		Aluno a4 = new Aluno("Marcelo Borges", 54234);
+//		Set<Aluno> alunos = javaColecoes.getAlunos();
+//		alunos.add(a4);
+		
+//		Set<String> nomes = Collections.emptySet();
+//      nomes.add("Paulo");
 
 	}
 
