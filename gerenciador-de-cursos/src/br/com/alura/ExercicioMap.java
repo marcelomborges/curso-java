0a5a6cb466aadbe731d2c2a998d91b3b3b5d76f3
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-11 11:37:19 -0300

    Java - Exercício Map

--- gerenciador-de-cursos/src/br/com/alura/ExercicioMap.java
+++ gerenciador-de-cursos/src/br/com/alura/ExercicioMap.java
@@ -1,9 +1,23 @@
 package br.com.alura;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ExercicioMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Map<Integer, String> pessoas = new HashMap<>();
+		Map<Integer, String> pessoas = new LinkedHashMap<>();
+
+		pessoas.put(21, "Leonardo Cordeiro");
+		pessoas.put(27, "Fabio Pimentel");
+		pessoas.put(19, "Silvio Mattos");
+		pessoas.put(23, "Romulo Henrique");
+
+		for (Integer idade : pessoas.keySet()) {
+			System.out.println(pessoas.get(idade));
+		}
 
 	}
 
