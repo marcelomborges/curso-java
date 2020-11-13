2a74b2f74fb7cfc0cefba921f34d8f543340382d
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-13 14:37:00 -0300

    Java - Configurando Dependências no Projeto Web

--- lojaweb/src/main/java/lojaweb/ContatoServlet.java
+++ lojaweb/src/main/java/lojaweb/ContatoServlet.java
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.maven.Produto;
+
 @WebServlet(urlPatterns= {"/contato"})
 public class ContatoServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		PrintWriter writer = resp.getWriter();
+		Produto produto = new Produto("DMC", 666.666);
 		writer.println("<html><h2>Bom dia</h2></html>");
 		writer.close();
 	}
