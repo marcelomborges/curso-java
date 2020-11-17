2be8ca2b1a567d8d8cb157f4aa910754357c4849
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-17 14:41:40 -0300

    Java - Criando o modelo Empresa para trabalhar com Servlets

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -17,11 +17,18 @@ public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	//protected void service(HttpServletRequest request, HttpServletResponse response)
-	//protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	//protected void doG(HttpServletRequest request, HttpServletResponse response)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("-> Cadastrando Nova Empresa");
-		PrintWriter out = response.getWriter();
+		
 		String nomeEmpresa = request.getParameter("nome");
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		PrintWriter out = response.getWriter();
 		out.println("<html><body>- Empresa " + nomeEmpresa + " Cadastrada com sucesso!</body></html>");
 		
 		
