cf0b4f4fc15136aee5d31548502e0b65a9754527
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-17 15:03:50 -0300

    Java - Passando informações para o html, e bloco de código estático em classes

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,18 @@ public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
 	
+
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		
+		lista.add(empresa1);
+		lista.add(empresa2);
+	
+	}
+	
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
 	}
