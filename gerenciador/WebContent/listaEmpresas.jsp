af567267f99ad89fbbd916a71b8a0060deea6910
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-19 15:24:17 -0300

    Java - Reorganizando os códigos para ter somente um Servlet Controller

--- gerenciador/WebContent/listaEmpresas.jsp
+++ gerenciador/WebContent/listaEmpresas.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa" %>
+<%@ page import="java.util.List,br.com.alura.gerenciador.modelo.Empresa" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
@@ -22,8 +22,8 @@
 				<fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy" var="data"/>
 				<li>
 					${empresa.nome} - ${data} - 
-					<a href="/gerenciador/mostraEmpresa?id=${empresa.id}">edita</a> - 
-					<a href="/gerenciador/removeEmpresa?id=${empresa.id}">remove</a>
+					<a href="/gerenciador/entrada?acao=MostraEmpresa&id=${empresa.id}">edita</a> - 
+					<a href="/gerenciador/entrada?acao=RemoveEmpresa&id=${empresa.id}">remove</a>
 				</li>
 				
 			</c:forEach>
--- gerenciador/src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,21 +6,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AlteraEmpresaServlet
- */
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-System.out.println("-> Alterando Empresa");
+public class AlteraEmpresa {
+	
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("-> Alterando empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");	
@@ -43,7 +39,7 @@ System.out.println("-> Alterando Empresa");
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
 		
-		response.sendRedirect("listaEmpresas");
+		response.sendRedirect("entrada?acao=ListaEmpresas");
 	}
 
 }
