c37897763617e4cea4e1e2b133b9ca93506abad0
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-24 11:50:39 -0300

    Java - Inserindo Conta no MariaDB

---
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/CriaConta.java
@@ -0,0 +1,29 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+		
+		Conta conta = new Conta();
+		conta.setTitular("Marcelo");
+		conta.setNumero(666);
+		conta.setAgencia(777);
+		
+		em.getTransaction().begin();
+		
+		em.persist(conta);
+		
+		em.getTransaction().commit();
+	}
+
+}
