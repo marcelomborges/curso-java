4f939a26d656f49277128c4ede9749fabe460004
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-24 13:12:46 -0300

    Java - Testando relacionamento Muitos para Muitos

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.alura.jpa.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -31,6 +33,8 @@ public class Movimentacao {
 	private BigDecimal valor;
 	@ManyToOne
 	private Conta conta;
+	@ManyToMany
+	private List<Categoria> categorias;
 	
 	public Long getId() {
 		return id;
@@ -62,4 +66,11 @@ public class Movimentacao {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+	
 }
