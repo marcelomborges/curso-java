b76c007867b6bc301dc9570863956ab2546b29c5
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-26 12:03:20 -0300

    Java - Problema com JPQL

--- projeto-jpa-2/src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
+++ projeto-jpa-2/src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDao.java
@@ -4,11 +4,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class MovimentacaoDao {
 	private EntityManager em;
@@ -16,6 +15,25 @@ public class MovimentacaoDao {
 	public MovimentacaoDao(EntityManager em) {
 		this.em = em;
 	}
+	
+//	public List<Movimentacao> getMovimentacoesFiltradasPorData(Integer dia, Integer mes, Integer ano) {
+//		
+//		String sql = "select * from Movimentacao m";
+//		
+//		if(dia != null) {
+//			sql = sql + " where day(m.data) =:pDia";
+//		}
+//		
+//		if(mes != null) {
+//			sql = sql + " and month(m.data) =:pMes";
+//		}
+//		
+//		if(ano != null) {
+//			sql = sql + " and year(m.data) =:pAno";
+//		}
+//		
+//		return null;
+//	}
 
 	public List<MediaComData> getMediaDiariaDasMovimentacoes() {
 		TypedQuery<MediaComData> query = em.createNamedQuery("mediaDiariaMovimentacoes", MediaComData.class);
