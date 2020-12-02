8bc4758f1640a8bfb3f7f907ca890dcf6f9821b7
Marcelo M. Borges <marcelomborges@outlook.com>
2020-12-02 11:35:44 -0300

    Java - Saída na tela no formato JSON

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -77,6 +78,13 @@ public class ProdutosController {
 		Produto produto = produtoDao.find(id);
 		modelAndView.addObject("produto", produto);
 		return modelAndView;
+	}
+	
+	@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto detalheJson(@PathVariable("id") Integer id) {
+		Produto produto = produtoDao.find(id);
+		return produto;
 	}
 
 	public ProdutoDAO getProdutoDao() {
