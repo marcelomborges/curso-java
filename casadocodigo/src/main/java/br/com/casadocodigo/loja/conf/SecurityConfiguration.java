d9d4f659cc3b199af1861cdcad49840b8ae56e37
Marcelo M. Borges <marcelomborges@outlook.com>
2020-12-03 10:05:25 -0300

    Java - Liberando a segurança de partes do site

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,9 +1,24 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
+@SuppressWarnings("deprecation")
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/produtos/form").hasRole("ADMIN")
+			.antMatchers("/carrinho").permitAll()
+			.antMatchers(HttpMethod.POST,"/produtos").hasRole("ADMIN")
+			.antMatchers(HttpMethod.GET,"/produtos").hasRole("ADMIN")
+			.antMatchers("/produtos/**").permitAll()
+			.antMatchers("/").permitAll()
+			.anyRequest().authenticated()
+			.and().formLogin();
+		
+	}
 }
